d0e0242cce9ebbd1fedca80d97292a186a170712
alicia-athub <laoise.maloney.2022@mumail.ie>
2024-12-04 16:46:27 +0000

    Imported pygame-widgets module to create a dropdown menu

--- FloodModel.py
+++ FloodModel.py
@@ -4,6 +4,9 @@
 #Last update: 19/11/24
 import pygame
 import numpy as np
+import pygame_widgets
+from pygame_widgets.button import Button
+from pygame_widgets.dropdown import Dropdown
     
 #Set up screen
 pygame.init()
@@ -18,23 +21,45 @@ grid = np.zeros((height, width), dtype=float)
 de_grid = np.zeros((height, width), dtype=float) # grid for elevation map
 #grid[10][9]= 1
 cx,cy= width/2, height/2   #location of mountain peak
+lx, ly = width/4, height/4  #location of lake
 l = 0 #distance from mountain peak
 c = width/4  # standard deviation, steepness of mountain 
 a = 1 #max. height of mountain
 #de_grid[10][9] = 1
-#Create mountain
+#Create mountain 3/12/24
 for y in range(height):
         for x in range(width):
             l = np.sqrt(((cx-x) * (cx-x)) + ((cy-y) * (cy-y))) # distance x, y is from mountain peak
             f = 1 * np.exp(-0.5*((l/c)*(l/c))) # height of mountain at x, y
             de_grid[x][y] = f
-#Create lake
+#Create lake 3/12/24
 for y in range(height-100):
         for x in range(width-100):
             l = np.sqrt(((cx-x) * (cx-x)) + ((cy-y) * (cy-y))) # distance x, y is from mountain peak
             f = 1 * (1 - np.exp(-0.5*((l/c)*(l/c)))) # height of mountain at x, y
             de_grid[x][y] = f + 10
             
+dropdown = Dropdown(
+    name='Select Color',
+    choices=[
+        'Red',
+        'Blue',
+        'Yellow',
+    ],
+    borderRadius=3, colour=pygame.Color('green'), values=[1, 2, 'true'], direction='down', textHAlign='left'
+)
+
+
+def print_value():
+    print(dropdown.getSelected())
+
+
+button = Button(
+    text='Print Value', fontSize=30,
+    margin=20, inactiveColour=(255, 0, 0), pressedColour=(0, 255, 0),
+    radius=5, onClick=print_value, font=pygame.font.SysFont('calibri', 10),
+    textVAlign='bottom'
+)
 
 #Button properties
 font = pygame.font.Font(None, 50)
@@ -56,7 +81,8 @@ running = True
 clock = pygame.time.Clock()
 while running:
     #Event manager
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT: #ON QUIT
             running = False
     #If mouse is clicked, call the on_mouse_button_down() function
@@ -77,10 +103,15 @@ while running:
     #Update de_grid
     for y in range(height):
         for x in range(width):
-            gs = int ((de_grid[x][y]) * 255)
-            color = pygame.Color(gs, gs, gs, 255)
+                    if x and y != 0:
+                        if lx/x == 1 and ly/y ==1: #if we are in the region of the lake, outline it with white to make it apparent
+                        # gs = int ((de_grid[x][y]) * 255)
+                         de_grid[x][y] = 0
+                
+                    gs = int ((de_grid[x][y]) * 255)
+                    color = pygame.Color(gs, gs, gs,55) 
 
-            pygame.draw.rect(screen, color, (800+(x * cell_size), 20+(y * cell_size), cell_size, cell_size))
+                    pygame.draw.rect(screen, color, (800+(x * cell_size), 20+(y * cell_size), cell_size, cell_size))
 
 
     #Display button text
@@ -89,6 +120,7 @@ while running:
 
     button_surface_el.blit(text_el, text_rect_el)
     screen.blit(button_surface_el, (button_rect_el.x, button_rect_el.y))
+    pygame_widgets.update(events)
     pygame.display.flip() #Double buffer reveal updated screen
 
     #Save the current grid
